75094e1cdc0b4903e13cfa82d8841c48ce59d4fe
Rafael-SV <delavega@rafaelsilverman.com>
2021-06-02 10:23:53 -0700

    first github commit

--- scripts/add_atoms_all_pdb.py
+++ scripts/add_atoms_all_pdb.py
@@ -1,4 +1,5 @@
 import subprocess
+import pickle
 import os
 import sys
 import pdbfixer
@@ -18,11 +19,16 @@ repeat
 """
 
 def remove_extra_chains(pdbfixer_obj):
+    """
+    calls some openMM functions
+    uses comprehension since the pdb fixer object is an interator
+    """
     num_chains = sum(1 for i in pdbfixer_obj.topology.chains())
     pdbfixer_obj.removeChains(range(1, num_chains))
     return pdbfixer_obj
 
 def add_missing_atoms(pdbfixer_obj):
+    """calls more openMM functions"""
     pdbfixer_obj.findMissingResidues()
     pdbfixer_obj.findMissingAtoms()
     pdbfixer_obj.addMissingAtoms()
@@ -31,6 +37,7 @@ def add_missing_atoms(pdbfixer_obj):
 # collect pdb file names in list
 directory_content = []
 
+#turn the following into function during refactoring
 scndr_obj = os.scandir('../pdb_files')
 for item in scndr_obj :
     pdb = item.name
@@ -44,14 +51,16 @@ missing_atom_dict = {}
 for pdb_name in directory_content:
     file_path = '../pdb_files/' + pdb_name
     fix_pdb = pdbfixer.PDBFixer(filename=file_path)
-    remove_extra_chains(fix_pdb)
-    add_missing_atoms(fix_pdb)
-    number_missing_atoms = sum(1 for i in fix_pdb.missingAtoms())
-    missing_atom_dict[pdb_file_path] = number_missing_atoms
-    output_file_name = str('fixed_' + pdb_file_path)
+    fix_pdb = remove_extra_chains(fix_pdb)
+    fix_pdb = add_missing_atoms(fix_pdb)
+    number_missing_atoms = len(fix_pdb.missingAtoms)
+    missing_atom_dict[pdb_name] = number_missing_atoms
+    output_file_name = str('fixed_' + pdb_name)
     with open(output_file_name, 'w') as output_pdb:
-        fix_pdb.writeFile(fix_pdb.topology, fix_pdb.positions, output_pdb)
-with open('pdb_name_and_number_missing_atoms_dict.pkl', 'w') as f:
+        PDBFile.writeFile(fix_pdb.topology, fix_pdb.positions, output_pdb)
+    break
+
+with open('pdb_name_and_number_missing_atoms_dict.pkl', 'wb') as f:
     pickle.dump(missing_atom_dict, f)
 
 
